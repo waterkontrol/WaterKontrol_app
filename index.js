48c2b03602e650d2ba3645920f8443e52eadc24e
waterkontrol <waterkontrol.info@gmail.com>
2025-11-13 11:33:33 -0400

    Se activa el cors nuevamente

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 // ✅ CORS explícito para evitar bloqueos en frontend
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(200);

5d9297596e1e496cf9460b49c685099c8537ea7b
waterkontrol <waterkontrol.info@gmail.com>
2025-12-02 16:17:51 -0400

    parametro correcto

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ app.post('/api/dispositivo/parametros', async (req, res) => {
 
 // POST /api/dispositivo/registro (Registrar dispositivo)
 app.post('/api/dispositivo/registro', async (req, res) => {
-  const { serial, nombre, userId } = req.body;
+  const { seriestype, nombre, userId } = req.body;
   if (!seriestype|| !nombre || !userId) {
     return res.status(400).json({ message: 'Datos incompletos.' });
   }

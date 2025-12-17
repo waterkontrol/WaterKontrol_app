8249cc6d2ac07957127f75e59d316084de1f3a29
waterkontrol <waterkontrol.info@gmail.com>
2025-12-16 22:49:11 -0400

    update registro valor

--- index.js
+++ index.js
@@ -442,6 +442,17 @@ const procesarMensajesMqtt = () => {
       `;
       await dbClient.query(updateDevice, [timestamp, rgt_id]);
 
+      const result1 = await pool.query(`SELECT vlr_id, tipo
+        FROM registro_valor 
+        JOIN parametros ON registro_valor.prt_id = parametros.prt_id
+        WHERE registro_valor.rgt_id = $1`, [rgt_id]); 
+
+      for(const row of result1.rows){
+        const insertQueryVal = `
+          UPDATE registro_valor SET valor = $3 WHERE rgt_id = $1 AND prt_id = $2;`;
+        const resultVal = await dbClient.query(insertQueryVal, [rgt_id, row.prt_id, message[row.tipo]]);
+      }
+
       await dbClient.query('COMMIT');
 
       admin.messaging().send({...msg, token: frb_token})
